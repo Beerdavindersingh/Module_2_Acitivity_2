a771b5fc30603b232fd7b0f7bd3fade23244fd76
Beerdavindersingh <beerdavindersingh@rrc.com>
2025-02-07 14:41:10 -0600

    Completed rectangle class

--- shape/rectangle.py
+++ shape/rectangle.py
@@ -8,9 +8,17 @@ import math
 
 class Rectangle(Shape):
     """
+    The Rectangle class represents a rectangle shape.
     """
     def _init_(self, color: str, length:int, width:int):
         """
+        Args:
+            color (str): The color of the rectangle.
+            length (int): The length of the rectangle.
+            width (int): The width of the rectangle.
+        
+        Raises:
+            ValueError: If length or width is not a number.
         """
         super()._init_(color)
         if not isinstance(length, int):
@@ -23,6 +31,7 @@ class Rectangle(Shape):
 
     def _str_(self) -> str:
         """
+        Returns a string.
         """
         value = super()._str_()
         value+= f"\nThis rectangle has four sides with the lengths of {self._length}, {self._width}, {self._length} and {self._width} centimeters."
@@ -30,10 +39,12 @@ class Rectangle(Shape):
     
     def calculate_area(self) -> float:
         """
+        Calculates the area of the rectangle.
         """
         return self._length * self._width
     
     def calculate_perimeter(self) -> float:
         """
+        Calculates the perimeter of the rectangle.
         """
         return 2 * self._length + 2 * self._width
